d1b520668b7e067c2caed8f35f3ef49d234d1d2c
Sudipta-Nandan-Aich <aichsudipta16@gmail.com>
2021-01-22 16:29:47 +0600

    upone

--- assignment.js
+++ assignment.js
@@ -1,1 +1,17 @@
 // https://github.com/Sudipta-Nandan-Aich/Javascript-Assignment
+
+// Problem: 1- kilometerToMeter
+
+function kilometerToMeter(input){
+    if(input < 0){
+        return "Distance can't be negative";
+    }
+    else if(typeof input =='string'){
+        return "Value of Kilometer should be numeric";
+    }
+    else{
+        var result = input * 1000;
+        return result;
+    }
+}
+
